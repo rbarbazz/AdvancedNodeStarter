9828ce4a6fad6af74cf839c1b4528cfcc288a29e
rbarbazz <42906704+rbarbazz@users.noreply.github.com>
2021-02-14 19:00:26 -0800

    feat: section 5 85-98

--- tests/header.test.js
+++ tests/header.test.js
@@ -1,5 +1,8 @@
 const puppeteer = require('puppeteer');
 
+const sessionFactory = require('./factories/sessionFactory');
+const userFactory = require('./factories/userFactory');
+
 let browser, page;
 
 beforeEach(async () => {
@@ -23,4 +26,18 @@ test('clicking login starts the oauth flow', async () => {
   await page.click('.right a');
 
   expect(page.url()).toMatch(/accounts\.google\.com/);
+});
+
+test('When signed in, shows logout button', async () => {
+  const user = await userFactory();
+  const { session, sig } = sessionFactory(user);
+
+  await page.setCookie({ name: 'session', value: session });
+  await page.setCookie({ name: 'session.sig', value: sig });
+  await page.goto('localhost:3000');
+  await page.waitFor('a[href="/auth/logout"]');
+
+  const text = await page.$eval('a[href="/auth/logout"]', el => el.innerHTML);
+
+  expect(text).toEqual('Logout');
 });
---
+++ tests/setup.js
@@ -0,0 +1,12 @@
+require('../models/User');
+
+const mongoose = require('mongoose');
+const keys = require('../config/keys');
+
+mongoose.Promise = global.Promise;
+
+beforeAll(() => {
+  mongoose.connect(keys.mongoURI, { useMongoClient: true });
+});
+
+afterAll(() => mongoose.disconnect());
